a97ec71cb8508b199a4e09d39cc9e6007cc17457
Jessica Ferreira <jessiberteloni@gmail.com>
2022-09-21 18:00:09 -0300

    Funções (def) em python - Parte 2

---
+++ exercicios/aula02.py
@@ -0,0 +1,33 @@
+"""
+  Funções - def em python - return - (Parte 2)
+"""
+# Exemplo 1
+def divisao(n1, n2):
+    if n2 == 0:
+        return
+
+    return n1 / n2
+
+divide = divisao(8,4)
+
+if divide:
+    print(divide)
+else:
+    print('Conta inválida.')
+print()
+
+# Exemplo 2
+def dumb():
+    return 1.1
+
+var = dumb()
+print(var, type(var))
+print()
+
+# Exemplo 3
+def dumb():
+    return ('Luiz', 'Otávio')
+
+var = dumb()
+
+print(var, type(var))
