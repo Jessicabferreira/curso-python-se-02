17230580793ae0d12e773edfa96141f231b88089
Jessica Ferreira <jessiberteloni@gmail.com>
2022-09-22 18:59:16 -0300

    Exercicios simples para entender as funções 1

--- exercicios/aula03.py
+++ exercicios/aula03.py
@@ -1,7 +1,7 @@
 """
       Exemplos simples para entender as funçoes
 
-1 - Crie uma função que exibe uma saudação com os parâmetros saudacao e nome.
+1 - Crie uma função que exibe uma saudação com os parâmetros saudação e nome.
 """
 
 def saudacao(saudacao, nome):
