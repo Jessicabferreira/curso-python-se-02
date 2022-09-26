c361d3bc0bdde2fa5ec00c64b989af58d7099bed
Jessicabferreira <jessiberteloni@gmail.com>
2022-09-26 18:49:10 -0300

    Tuplas em python

---
+++ exercicios/aula07.py
@@ -0,0 +1,9 @@
+"""
+ Tuplas em python
+Tupla é semelhante a lista. Porém, ela é imutável, ou seja, após uma
+tupla ser criada, ela não pode ser alterada.
+"""
+receita = ('Farinha', 'Açucar', 'Manteiga')  # tuplas usa parênteses ()
+print(type(receita))    # caso a var tenha somente 1 item usar , depois dele para não passar por str
+print(receita)
+print(receita[2])  # acessar o itm por meio da índice
