80c0d86c19569a4091779525500ada2d2ed7a6b6
Jessica Ferreira <jessiberteloni@gmail.com>
2022-10-04 19:42:34 -0300

    Zip e Zip_longest - Unindo iteráveis

--- exercicios/aula16.py
+++ exercicios/aula16.py
@@ -1,13 +1,13 @@
 """
-Zip - Unido iteráveis - não precisa importar nada para utilizar
-Zip_longest - Itertools
+Zip - Unindo iteráveis - não precisa importar nada para utilizar
+Zip_longest - Itertools - precisa ser importado
 """
 
 # Exemplo 1 - com zip
-cidades = ['São Paulo', 'Belo Horizonte', 'Salvador', 'Monte Belo']  # Monte Belo será ignorado por não ter o estado definido
+cidades = ['São Paulo', 'Belo Horizonte', 'Salvador', 'Monte Belo']  # Monte Belo será ignorado por não ter estado definido
 estados = ['SP', 'MG', 'BA']
 
-cidades_estados = zip(cidades, estados)  # zip juntou as duas listas em uma unica lista
+cidades_estados = zip(cidades, estados)  # zip uniu as duas listas em uma unica lista
 print(list(cidades_estados))
 print()
 # outra opçao com for
@@ -16,9 +16,9 @@ print()
 
 # Exemplo 2 - com zip_longest
 from itertools import zip_longest
-cidades = ['São Paulo', 'Belo Horizonte', 'Salvador', 'Monte Belo']  # Vai incluir e preencher com None
+cidades = ['São Paulo', 'Belo Horizonte', 'Salvador', 'Monte Belo']
 estados = ['SP', 'MG', 'BA']
 
-cidades_estados = zip_longest(cidades, estados)
+cidades_estados = zip_longest(cidades, estados)  # inclui o Monte Belo e preenche com None por não ter estado definido
 print(list(cidades_estados))
 
